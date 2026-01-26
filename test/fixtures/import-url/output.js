b8d5824c768c28952006e9af248c70a34f605d8a
Daniel Kostro <kostro.d@gmail.com>
2026-01-26 15:13:10 +0100

    fix: do not replace .js extension on URLs

---
+++ test/fixtures/import-url/output.js
@@ -0,0 +1,8 @@
+"use strict";
+
+define(["https://foo.com/lib/foo.js"], function (_fooModule) {
+  let _foo = _interopRequireDefault(_fooModule);
+  function _interopRequireDefault(e) {
+    return e && e.__esModule ? e : { default: e };
+  }
+});
